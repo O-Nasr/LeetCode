6f96b4cc3719a48aeff4b281338902a3337335e4
Omar Nasr Saad <o.nasr1234@gmail.com>
2021-12-08 00:31:26 +0200

    Add project with array folder

---
+++ LeetCode/Program.cs
@@ -0,0 +1,15 @@
+﻿using LeetCode.Array;
+using System;
+
+namespace LeetCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 2, 7, 11, 15 };
+            var x = Two_Sum.TwoSum(nums, 9);
+            Console.WriteLine(x);
+        }
+    }
+}
