0fe6df05b5b3cc074fdc856be4ac226f8e5dca68
Omar Nasr Saad <o.nasr1234@gmail.com>
2021-12-08 00:31:54 +0200

    Add Two Sum problem

---
+++ LeetCode/Array/Two_Sum.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LeetCode.Array
+{
+    public class Two_Sum
+    {
+        //https://leetcode.com/problems/two-sum/
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            int[] result = new int[2];
+            IDictionary<int,int> dec = new Dictionary<int, int>();
+            int second = 0;
+
+            for(int i = 0; i < nums.Length; i++)
+            {
+                second = target - nums[i];
+                if (dec.ContainsKey(second))
+                {
+                    result.SetValue(i, 0);
+                    result.SetValue(dec[second], 1);
+                    break;
+                }
+                else
+                {
+                    if (!dec.ContainsKey(nums[i]))
+                    {
+                        dec.Add(nums[i], i);
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
