dcbbe67e8f417d77195a76eafae365e95ec1ed1c
Omar Nasr Saad <o.nasr1234@gmail.com>
2021-12-08 22:49:06 +0200

    Contains Duplicate

---
+++ LeetCode/Array/Contains_Duplicate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Array
+{
+    public class Contains_Duplicate
+    {
+        public static bool ContainsDuplicate(int[] nums)
+        {
+            if (nums == null) return false;
+
+            HashSet<int> set = new HashSet<int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (set.Contains(nums[i]))
+                {
+                    return true;
+                }
+                else
+                {
+                    set.Add(nums[i]);
+                }
+            }
+            return false;
+        }
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -7,9 +7,21 @@ namespace LeetCode
     {
         static void Main(string[] args)
         {
-            int[] nums = { 2, 7, 11, 15 };
-            var x = Two_Sum.TwoSum(nums, 9);
+            int[] nums = { 1, 2, 3, 4 };
+            //var x = Two_Sum.TwoSum(nums, 9);
+
+            var x = Contains_Duplicate.ContainsDuplicate(nums);
             Console.WriteLine(x);
+
+            //DisplayCollection(x);
+        }
+
+        public static void DisplayCollection(int[] arr)
+        {
+            foreach(int i in arr)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
