51fbba5e9b9b3fbc2d12d5a02b8b46540a5ca93a
Omar Nasr Saad <o.nasr1234@gmail.com>
2021-12-10 14:43:09 +0200

    Find Minimum in Rotated Sorted Array

---
+++ LeetCode/Array/Find_Minimum_In_Rotated_Sorted_Array.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCode.Array
+{
+    public class Find_Minimum_In_Rotated_Sorted_Array
+    {
+        //https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
+        public int FindMin(int[] nums)
+        {
+            if(nums == null || nums.Length == 0) { return -1; }
+            if(nums.Length == 1) { return nums[0]; }
+
+            int left = 0;
+            int right = nums.Length - 1;
+            int midPoint = 0;
+
+            while (left < right) { 
+                midPoint = left + (right - left) / 2;
+                if (midPoint > 0 && nums[midPoint] < nums[midPoint - 1])
+                {
+                    return nums[midPoint];
+                }
+                if (nums[midPoint] >= nums[left] && nums[midPoint] > nums[right]) //go right, otherwise go left
+                {
+                    left = midPoint + 1;
+                }
+                else
+                {
+                    right = midPoint - 1;
+                }
+            }
+
+            return nums[left];
+        }
+    }
+}
